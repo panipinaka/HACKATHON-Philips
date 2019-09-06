ac7073955b033f28d80c9eac2ecd09dc30dfa257
panipinaka <pinakapani1@gmail.com>
2019-09-06 16:20:19 +0530

    calculated nan percentage

--- extract.py
+++ extract.py
@@ -7,17 +7,17 @@ import os
 path = r'E:\new' 
 all_files = glob.glob(path + "/*.psv")
 f_name=os.path.splitext(os.path.basename("p000001.psv"))[0]
-data_collection = pd.read_csv("p000001.psv",sep='|',index_col=None, header=0)                 #cantains all file name
-data_collection['hours']=data_collection.index                                              #adding hours coloumn
+data_collection = pd.read_csv("p000001.psv",sep='|',index_col=None, header=0)                 
+data_collection['hours']=data_collection.index                                              
 data_collection['identifier']=f_name 
 ##data_collection.loc['NAN_VALUE'] =(data_collection.isna().sum()/data_collection.shape[0])*100
 #print(data_collection)
 
 for filename in all_files:  
-    if filename!="p000001.psv":                                                #loop over the filenames
+    if filename!="p000001.psv":                                                                             
         data = pd.read_csv(filename,sep='|',index_col=None, header=0)
-        f_name=os.path.splitext(os.path.basename(filename))[0]                 #filename without extintion file
-        data['hours']=data.index                                              #adding hours coloumn
+        f_name=os.path.splitext(os.path.basename(filename))[0]                 
+        data['hours']=data.index
         data['identifier']=f_name  
         data_collection = data_collection.append(data)
 new=data_collection.copy()  
